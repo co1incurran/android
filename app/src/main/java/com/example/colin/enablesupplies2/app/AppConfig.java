6b67f3d8eaa8e54fb98f1bdc7ab4ffc6e7b65ef7
Colin Curran <col.w.curran@gmail.com>
2016-04-05 16:42:56 +0100

    Ialmost complete login and signup. Started work on UI components

---
+++ app/src/main/java/com/example/colin/enablesupplies2/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.colin.enablesupplies2.app;
+
+/**
+ * Created by colin on 05/04/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://127.0.0.1/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://127.0.0.1/android_login_api/register.php";
+}
